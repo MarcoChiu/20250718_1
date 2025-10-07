c23a845e816c7e74aa026c0f743fa0bd6fda7712
Marco Chiu <marco79mo@gmail.com>
2025-10-07 15:59:13 +0800

    改config

--- vite.config.js
+++ vite.config.js
@@ -25,7 +25,7 @@ function moveOutputPlugin() {
 export default defineConfig({
   // base 的寫法:
   // base: '/Repository 的名稱/'
-  base: '/20250718_demo/',
+  base: '/20250718_1/',
   plugins: [
     liveReload(['./layout/**/*.ejs', './pages/**/*.ejs', './pages/**/*.html']),
     ViteEjsPlugin(),
